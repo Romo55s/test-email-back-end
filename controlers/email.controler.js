ef72a43ce8e45ca5df35ee813a04c7848e207b03
Tony <pp.tony777@gmail.com>
2023-05-25 00:01:56 -0600

    small change for response on the front-end

--- controlers/email.controler.js
+++ controlers/email.controler.js
@@ -1,39 +1,40 @@
-const {} = require('express');
+const { request, response } = require('express');
 const nodeMailer = require('nodemailer');
 
+// Funcion para mandar el correo
+const sendEmail = (req = request, res = response) => {
+  let body = req.body;
 
-// Funcion para mandar el correo 
-const sendEmail = (req=request,resp=response) =>{
-    let body = req.body;
+  // Protocolo por el cual se va a mandar el correo y la cuenta desde la cual se enviará
+  let config = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+      user: 'test9072686@gmail.com',
+      pass: 'alratqniiglkgwdo'
+    }
+  });
 
-    // Protocolo por el cual se va mandar el correo y la cuenta la cual va mandar el correo
-    let config = nodeMailer.createTransport({
-        host: 'smtp.gmail.com',
-        post: 587,
-        auth:{
-            user: 'test9072686@gmail.com',
-            pass: 'alratqniiglkgwdo'
-        }
+  // Contenido del correo
+  const options = {
+    from: 'Bisne',
+    subject: body.about,
+    to: body.email,
+    text: body.mssg
+  };
+
+  config.sendMail(options, function(error, result) {
+    console.log(options);
+    if (error) {
+      return res.json({ ok: false, msg: error });
+    }
+    return res.json({
+      ok: true,
+      msg: result
     });
-
-    // Contenido del correo
-    const options = {
-        from: 'Bisne',
-        subject: body.about,
-        to: body.email,
-        text: body.mssg
-    };
-
-    config.sendMail(options,function(error,result){
-        console.log(options);
-        if (error) return resp.json({ok: false, msg: error});
-        return resp.json({
-            ok:true,
-            msg:result
-        })
-    });
-}
+  });
+};
 
 module.exports = {
-    sendEmail
-}
+  sendEmail
+};
