35df2e4260267eb865dc79472078c32e28f6432c
Tony <pp.tony777@gmail.com>
2023-05-24 23:39:49 -0600

    shor change

--- server.js
+++ server.js
@@ -8,7 +8,3 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
 app.use(require('./routes/email.routes'));
-
-app.listen('3000', () =>{
-    console.log('Listening');
-});
